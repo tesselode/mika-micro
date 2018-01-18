a7fe85d1f24142c696c1ddf8ebe16f6f4f40fc32
Andrew Minnich <aminnich3@gmail.com>
2018-01-18 02:32:53 -0500

    clamp filter cutoff correctly

--- source/Filter.cpp
+++ source/Filter.cpp
@@ -4,11 +4,12 @@ double Filter::Process(double input, double cutoff)
 {
 	double smoothing = resonance < 0.0 ? -resonance : 0.0;
 	double res = resonance < 0.0 ? 0.0 : resonance;
+	cutoff = cutoff < 20.0 ? 20.0 : cutoff > 20000.0 ? 20000.0 : cutoff;
 	cutoff *= cutoffMultiplier;
 
 	f += (cutoff - f) * .01; // cutoff smoothing
-	double fClamped = f > 1.0 ? 1.0 : f < -1.0 ? -1.0 : f > 0.0 ? f * f : f < 0.0 ? -(f * f) : 0.0; // clamp and square
-	velocity += fClamped * (input - (value + velocity * (1 - res)));
+	double fSquare = f * f;
+	velocity += fSquare * (input - (value + velocity * (1 - res)));
 	if (smoothing > 0.0) velocity -= velocity * smoothing;
 	value += velocity;
 	value = atan(value * .5) * 2;
