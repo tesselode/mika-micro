189bc9c062b092deba17ba8664c8e225637ea9c7
Andrew Minnich <aminnich3@gmail.com>
2018-02-04 01:07:22 -0500

    init

---
+++ source/MikaMicro.h
@@ -0,0 +1,19 @@
+#ifndef __MIKAMICRO__
+#define __MIKAMICRO__
+
+#include "IPlug_include_in_plug_hdr.h"
+
+class MikaMicro : public IPlug
+{
+public:
+	MikaMicro(IPlugInstanceInfo instanceInfo);
+	~MikaMicro();
+
+	void Reset();
+	void OnParamChange(int paramIdx);
+	void ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames);
+
+private:
+};
+
+#endif
