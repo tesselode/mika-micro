d5bea15be215d85d2b1b2ca07796c829c8a30d7d
Andrew Minnich <aminnich3@gmail.com>
2018-02-23 00:01:54 -0500

    boop

--- source/Util.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <math.h>
-
-double pitchFactor(double p);
-double pitchToFrequency(double p);
-double lerp(double a, double b, double f);
-unsigned long xorshf96();
-double fastSin(double x);
