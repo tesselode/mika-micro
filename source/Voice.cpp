1da1dafc16bdb46547d89b2d0ca196df5681a084
Andrew Minnich <aminnich3@gmail.com>
2018-04-09 03:26:34 -0400

    fm

--- source/Voice.cpp
+++ source/Voice.cpp
@@ -8,6 +8,7 @@ void Voice::SetNote(int n)
 
 void Voice::Start()
 {
+	oscFm.Reset();
 	osc1a.Reset();
 	osc1b.Reset(p[kOsc1Split] < 0.0 ? .33 : 0.0);
 	osc2a.Reset();
@@ -26,12 +27,38 @@ void Voice::Release()
 
 double Voice::Next(double dt)
 {
+	// skip processing if voice is silent
 	volEnv.Update(dt, p[kVolEnvA], p[kVolEnvD], p[kVolEnvS], p[kVolEnvR]);
 	if (GetVolume() == 0.0) return 0.0;
 
+	// oscillator base frequencies
 	auto osc1Frequency = baseFrequency * osc1PitchFactor;
 	auto osc2Frequency = baseFrequency * osc2PitchFactor;
 
+	// fm
+	switch ((int)p[kFmMode])
+	{
+	case 0:
+		break;
+	case 1:
+	case 2:
+	{
+		auto fmAmount = p[kFmCoarse] + p[kFmFine];
+		auto fmValue = pitchFactor(oscFm.Next(dt, osc1Frequency, kSine) * fmAmount);
+		switch ((int)p[kFmMode])
+		{
+		case 1:
+			osc1Frequency *= fmValue;
+			break;
+		case 2:
+			osc2Frequency *= fmValue;
+			break;
+		}
+		break;
+	}
+	}
+
+	// oscillator 1
 	auto osc1Out = 0.0;
 	if (p[kOscMix] < 1.0)
 	{
@@ -40,6 +67,7 @@ double Voice::Next(double dt)
 			osc1Out += osc1b.Next(dt, osc1Frequency / (1.0 + p[kOsc1Split]), (EWaveforms)(int)p[kOsc1Wave]);
 	}
 
+	// oscillator 2
 	auto osc2Out = 0.0;
 	if (p[kOscMix] > 0.0)
 	{
@@ -48,6 +76,7 @@ double Voice::Next(double dt)
 			osc2Out += osc2b.Next(dt, osc2Frequency / (1.0 + p[kOsc2Split]), (EWaveforms)(int)p[kOsc2Wave]);
 	}
 
+	// oscillator mix
 	auto out = osc1Out * (1.0 - p[kOscMix]) + osc2Out * p[kOscMix];
 	out /= (1.0 + abs(.5 - p[kOscMix])) * 1.5;
 
