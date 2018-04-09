c787ff2367972dc5cf9301d8a2d9be45399d7cb3
Andrew Minnich <aminnich3@gmail.com>
2018-04-09 03:12:51 -0400

    oscillator mix

--- source/Voice.cpp
+++ source/Voice.cpp
@@ -29,14 +29,24 @@ double Voice::Next(double dt)
 	volEnv.Update(dt, p[kVolEnvA], p[kVolEnvD], p[kVolEnvS], p[kVolEnvR]);
 	if (GetVolume() == 0.0) return 0.0;
 
-	auto out = 0.0;
-
+	auto osc1Out = 0.0;
 	if (p[kOscMix] < 1.0)
 	{
-		out += osc1a.Next(dt, baseFrequency * (1.0 + p[kOsc1Split]), (EWaveforms)(int)p[kOsc1Wave]);
+		osc1Out += osc1a.Next(dt, baseFrequency * (1.0 + p[kOsc1Split]), (EWaveforms)(int)p[kOsc1Wave]);
 		if (p[kOsc1Split] != 0.0)
-			out += osc1b.Next(dt, baseFrequency / (1.0 + p[kOsc1Split]), (EWaveforms)(int)p[kOsc1Wave]);
+			osc1Out += osc1b.Next(dt, baseFrequency / (1.0 + p[kOsc1Split]), (EWaveforms)(int)p[kOsc1Wave]);
 	}
+
+	auto osc2Out = 0.0;
+	if (p[kOscMix] > 0.0)
+	{
+		osc2Out += osc2a.Next(dt, baseFrequency * (1.0 + p[kOsc2Split]), (EWaveforms)(int)p[kOsc2Wave]);
+		if (p[kOsc2Split] != 0.0)
+			osc2Out += osc2b.Next(dt, baseFrequency / (1.0 + p[kOsc2Split]), (EWaveforms)(int)p[kOsc2Wave]);
+	}
+
+	auto out = osc1Out * (1.0 - p[kOscMix]) + osc2Out * p[kOscMix];
+	out /= (1.0 + abs(.5 - p[kOscMix])) * 1.5;
 
 	return out;
 }
