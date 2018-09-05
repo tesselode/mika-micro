3a405e1e444953d1e1f0a82e7533c1286a59ad99
Andrew Minnich <aminnich3@gmail.com>
2018-09-04 20:54:05 -0400

    recreate project

---
+++ source/MikaMicro.cpp
@@ -0,0 +1,83 @@
+#include "MikaMicro.h"
+#include "IPlug_include_in_plug_src.h"
+#include "IControl.h"
+#include "resource.h"
+
+const int kNumPrograms = 1;
+
+enum EParams
+{
+  kGain = 0,
+  kNumParams
+};
+
+enum ELayout
+{
+  kWidth = GUI_WIDTH,
+  kHeight = GUI_HEIGHT,
+
+  kGainX = 100,
+  kGainY = 100,
+  kKnobFrames = 60
+};
+
+MikaMicro::MikaMicro(IPlugInstanceInfo instanceInfo)
+  :	IPLUG_CTOR(kNumParams, kNumPrograms, instanceInfo), mGain(1.)
+{
+  TRACE;
+
+  //arguments are: name, defaultVal, minVal, maxVal, step, label
+  GetParam(kGain)->InitDouble("Gain", 50., 0., 100.0, 0.01, "%");
+  GetParam(kGain)->SetShape(2.);
+
+  IGraphics* pGraphics = MakeGraphics(this, kWidth, kHeight);
+  pGraphics->AttachPanelBackground(&COLOR_RED);
+
+  IBitmap knob = pGraphics->LoadIBitmap(KNOB_ID, KNOB_FN, kKnobFrames);
+
+  pGraphics->AttachControl(new IKnobMultiControl(this, kGainX, kGainY, kGain, &knob));
+
+  AttachGraphics(pGraphics);
+
+  //MakePreset("preset 1", ... );
+  MakeDefaultPreset((char *) "-", kNumPrograms);
+}
+
+MikaMicro::~MikaMicro() {}
+
+void MikaMicro::ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames)
+{
+  // Mutex is already locked for us.
+
+  double* in1 = inputs[0];
+  double* in2 = inputs[1];
+  double* out1 = outputs[0];
+  double* out2 = outputs[1];
+
+  for (int s = 0; s < nFrames; ++s, ++in1, ++in2, ++out1, ++out2)
+  {
+    *out1 = *in1 * mGain;
+    *out2 = *in2 * mGain;
+  }
+}
+
+void MikaMicro::Reset()
+{
+  TRACE;
+  IMutexLock lock(this);
+}
+
+void MikaMicro::OnParamChange(int paramIdx)
+{
+  IMutexLock lock(this);
+
+  switch (paramIdx)
+  {
+    case kGain:
+      mGain = GetParam(kGain)->Value() / 100.;
+      break;
+
+    default:
+      break;
+  }
+}
---
+++ source/MikaMicro.h
@@ -0,0 +1,20 @@
+#ifndef __MIKAMICRO__
+#define __MIKAMICRO__
+
+#include "IPlug_include_in_plug_hdr.h"
+
+class MikaMicro : public IPlug
+{
+public:
+  MikaMicro(IPlugInstanceInfo instanceInfo);
+  ~MikaMicro();
+
+  void Reset();
+  void OnParamChange(int paramIdx);
+  void ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames);
+
+private:
+  double mGain;
+};
+
+#endif
